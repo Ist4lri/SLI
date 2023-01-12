939f9ac3f701bf1b298a9f62f6921c02eccd7ddd
Ist4lri <pierreloic.gouttebel@gmail.com>
2023-01-12 16:26:09 +0100

    begin homework

--- TP1/README.md
+++ TP1/README.md
@@ -2,7 +2,7 @@ TP1
 
 Je m'appelle GOUTTEBEL Pierre-Loïc, je suis un étudiant en M1 DLAD, et je continue d'apprendre git. La raison est qu'on ne cesse jamais d'apprendre les choses
 
-On aura le fichier qu'on a fait en classe.
+On aura le fichier qu'on a fait en cours.
 Le corriger pour que Pylint donne une note de 10/10
 Créer dans Git un dossier TP2, et on l'upload sur Git
 Il vérifie que le fichier existe. Si il existe, il le fait, sinon, fichier suivant etc.
--- TP2/script.py
+++ TP2/script.py
@@ -1,1 +1,50 @@
-print("oui")
+# coding: utf-8
+import sys
+
+
+def chek_fasta_file(file):
+    with open(file, "r", encoding="utf-8") as read_file:
+        if read_file.readline()[0] != ">":
+            if read_file.readline()[0] == "# codin: utf-8":
+                return "script"
+            return "pass"
+        return file
+
+
+ADN_LIST = ("A", "C", "G", "T")
+
+
+def adn_read(fastafile):
+    with open(fastafile, "r", encoding="utf-8") as read_file:
+        counter = 0
+        header = ""
+        for line in read_file.readlines():
+            counter += 1
+            if line[0] == ">":
+                header = line.strip()
+            else:
+                line = line.strip()
+                line = line.upper()
+                column_counter = 0
+                for char in line:
+                    column_counter += 1
+                    if char not in ADN_LIST:
+                        print(char + " It's not a nucl in line " + str(counter) +
+                              " and column " + str(column_counter) + " for sequence "+header[1:])
+
+
+print(sys.argv)
+for arg in sys.argv[1:]:
+    print(chek_fasta_file(arg))
+
+
+# def pierre_louis():  # C'EST PIERRE-LOÏC #
+#     with open("a.fasta", "r") as mainFile:
+#         if mainFile.readline()[0] == ">":
+#             for line in mainFile.readlines():
+#                 for char in line:
+#                     if char != "A" or char != "C" or char != "G" or char != "T":
+#                         print("Error this is not a nucleotide !")
+#                         break
+#         else:
+#             print("This is not a fasta file !")
