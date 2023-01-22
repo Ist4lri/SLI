15a514aadd00b92525cb1f4a664e1f8c5713346c
Ist4lri <pierreloic.gouttebel@gmail.com>
2023-01-22 22:27:45 +0100

    ça marche pas

--- TP3/order_66.py
+++ TP3/order_66.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 """How to make a Zoo in POO ?"""
+from decimal import getcontext, Decimal
 
 
 class Animal():
@@ -26,16 +27,41 @@ class Animal():
                     species, new_generation)
                 print(f'Reproduction effectuée !')
 
-    def where_is_mummy(self) -> None:
+    def where_is_mummy(self, asking_generation) -> None:
         if self.children == {}:
             print(f"L'espèce {self.species} a besoin de se reproduire avant !")
+        else:
+            if asking_generation >= 2.0:
+                list_of_ancestor = []
+                counter = 0
+                getcontext().prec = 2
+                temp_ask_generation = Decimal(asking_generation)
+                while temp_ask_generation != 1:
+                    if ((temp_ask_generation - Decimal(0.01)) in self.children):
+                        counter += 1
+                        getcontext().prec = 2
+                        temp_ask_generation -= Decimal(0.01)
+
+                print("out")
+                while counter != 0:
+                    list_of_ancestor.append(
+                        self.children[asking_generation-0.01])
+                    asking_generation -= 0.01
+                    counter -= 1
+                print(list_of_ancestor)
+            else:
+                print(f'Ancêtre {self.children[1.0]}')
 
 
 if __name__ == "__main__":
     Chat = Animal("Chat")
-    Chat.where_is_mummy()
+    Chat.where_is_mummy(1.3)
     Chat.make_a_children("Chat", 1)
     Chat.make_a_children("Chat", 1)
-    Chat.make_a_children("Chat", 1)
-    Chat.make_a_children("Chat", 1)
+    Chat.make_a_children("Chat", 2)
+    Chat.make_a_children("Chat", 2)
+    Chat.make_a_children("Chat", 3)
+    Chat.make_a_children("Chat", 3)
+    Chat.where_is_mummy(2.06)
+
     print(Chat.children)
