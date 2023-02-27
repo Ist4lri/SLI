3d7563af7c44fb9c35777042ed35c56130ccf932
Ist4lri <pierreloic.gouttebel@gmail.com>
2023-02-27 16:45:44 +0100

    Going to finish, in 2 hours

--- TP6/controller.py
+++ TP6/controller.py
@@ -4,15 +4,15 @@ from model import Model
 
 class Controller():
     def __init__(self):
-
-        self.reading_file()
+        self.model = Model("a.txt", "r+")
+        self.model.read_file()
         self.view = Application(self)
         self.lb_display()
         self.view.view_window()
 
-    def reading_file(self):
-        self.model = Model("a.txt")
-        self.model.read_file()
+    def open_truncate_file(self, option):
+        self.model.close()
+        self.model = Model("a.txt", option)
 
     def lb_display(self):
         for key in self.model.dico_animaux.keys():
@@ -23,15 +23,14 @@ class Controller():
 
     def to_delete(self, key_dict):
         del self.model.dico_animaux[key_dict]
+        self.model.check_line(key_dict, "delete")
         self.view.resett_app()
         self.view.initiate_widgets()
         self.lb_display()
 
     def add_animal(self, dict_animal):
-        self.view.counter = 0
         self.model.save(dict_animal)
         self.view.refresh()
-        self.model.close()
         self.reading_file()
         self.lb_display()
 
--- TP6/model.py
+++ TP6/model.py
@@ -2,24 +2,34 @@ from Animal import Animal
 
 
 class Model():
-    def __init__(self, filename):
+    def __init__(self, filename, opening):
         self.filename = filename
-        self.file = open(self.filename, "r+")
+        self.file = open(self.filename, f'{opening}')
         self.dico_animaux = {}
 
     def read_file(self):
+        self.file.seek(0)
         for line in self.file:
             line = line.strip()
             tab = line.split(",")
             a = Animal(tab[0], tab[1], tab[2], tab[3], tab[4])
             self.dico_animaux[a.name] = a
 
+    def check_line(self, search, mode):
+        self.file.seek(0)
+        stock_file = self.file.read()
+        self.file.truncate()
+        for line in stock_file:
+            if search not in stock_file:
+                if mode == "delete":
+                    self.file.write(line)
+
+        # Le code ici est bien pour faire du remplacement, quand il faut modifier les entry.
+        # a trouver : Un moyen de supriomer ce qu'on veut pas.
+
     def save(self, dict_animal):
         self.file.write("\n"+dict_animal["species"]+","+dict_animal["age"]+"," +
                         dict_animal["diet"]+","+dict_animal["foot"]+","+dict_animal["name"])
-
-    def delete(self, name):
-        self.file
 
     def close(self):
         self.file.close()
